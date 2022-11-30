b04a73c07cf18d0bb1720056925ee9d0bc8d72fa
Robin <robin.kriisa@voco.ee>
2022-11-30 17:07:34 +0200

    As a user I can see meal names and their calories [delivers #183921879]

---
+++ app.js
@@ -0,0 +1,64 @@
+
+const ItemCtrl = (function (){
+    // Private
+    // ItemCtrl module construct
+    const Item = function (id, name, calories){
+        this.id = id
+        this.name = name
+        this.calories = calories
+    }
+    //data structure
+    const data = {
+        items:[
+            new Item(0, `Steak dinner`, 1200),
+            new Item(1, `Cookie`, 420),
+            new Item(0, `A egg`, 119),
+        ],
+        total: 0
+    }
+    //public methods
+    return{
+        logData: function(){
+            return data
+        },
+        getTotalCalories: function(){
+            let total = 0
+            data.items.forEach(function (item){
+                total = total + item.calories
+            })
+            data.total = total
+            return data.total
+        },
+        getItems: function (){
+            return data.items
+        }
+    }
+})();
+
+const UICtrl = (function(){
+    return{
+        populateitemList: function (items){
+            let html = ``
+            items.forEach(function(item){
+                html += `<li id="item-${item.id}"><strong>${item.name}:</strong><em>${item.calories}</em>Calories</li>`
+            })
+            document.querySelector(`ul`).innerHTML = html
+        },
+        showTotalCalories: function (totalCalories){
+            document.querySelector(`.total-calories`).textContent = totalCalories
+        }
+    }
+})
+
+const App = (function(){
+    return{
+        init: function(){
+            const items = ItemCtrl.getItems()
+            console.log(items)
+            UICtrl.populateItemList(items)
+            const totalCalories = ItemCtrl.getTotalCalories()
+            UICtrl.showTotalCalories(totalCalories)
+        }
+    }
+})(ItemCtrl)
+App.init()
